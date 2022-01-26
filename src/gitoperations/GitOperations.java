693ddcd4b819883666fee1b6f880d749d87f2761
ahmsyedd <98047590+ahmsyedd@users.noreply.github.com>
2022-01-26 12:59:08 -0500

    Update GitOperations.java

--- src/gitoperations/GitOperations.java
+++ src/gitoperations/GitOperations.java
@@ -15,6 +15,7 @@ public class GitOperations {
      */
     public static void main(String[] args) {
         // TODO code application logic here hey bye
+        System.out.println("Hey Man");
         
     }
     
